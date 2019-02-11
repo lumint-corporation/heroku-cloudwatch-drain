71cc57b67ce462da15aacedf8f752be8c84447e6
Matías Hernán García <garcia.matias92@gmail.com>
2019-02-11 16:23:49 -0300

    Remove ;

--- logparser/logparser.go
+++ logparser/logparser.go
@@ -20,14 +20,14 @@ type LogEntry struct {
 // Parse returns a parsed entry for a single Heroku syslog message delivered via
 // HTTPS.
 func Parse(b []byte) (*LogEntry, error) {
-	fmt.Println(b);
+	fmt.Println(b)
 	parser := logParser{
 		b:      b,
 		cursor: 0,
 		len:    len(b),
 	}
 	parsed := parser.parse()
-	fmt.Println(parsed);
+	fmt.Println(parsed)
 	return parsed
 }
 
